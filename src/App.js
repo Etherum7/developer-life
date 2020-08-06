775cd9bbdfcdef6d926f24821dba96b50a2ba9f4
Harsh Vardhan <hvs.231918@gmail.com>
2020-08-06 13:31:41 +0530

    First Day

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+//Node Modules
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+//Internal Modules
 import './App.css';
+import SignIn from './components/sign-in/sign-in.component';
+import SignUp from './components/sign-up/sign-up.component';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<Switch>
+					<Route exact path="/signin" component = {SignIn}/>
+					<Route exact path="/signup" component = {SignUp}/>
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
